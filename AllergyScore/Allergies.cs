c2e10ada5916be58f5343542831889617bc7fdbb
Дмитрий Шепелев <shepelev.dm@gmail.com>
2021-01-13 16:58:19 +0300

    Done

--- AllergyScore/Allergies.cs
+++ AllergyScore/Allergies.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AllergyScore
 {
@@ -8,15 +9,23 @@ namespace AllergyScore
     public class Allergies
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="Allergies"/> class.
         /// Initializes the allergies object on base of test score.
         /// </summary>
         /// <param name="score">The allergy test score.</param>
         /// <exception cref="ArgumentException">Thrown when score is less than zero.</exception>
         public Allergies(int score)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (score < 0)
+            {
+                throw new ArgumentException($"Size of matrix '{score}' cannot be less than zero.");
+            }
+
+            this.Score = score;
         }
-        
+
+        public int Score { get; private set; }
+
         /// <summary>
         /// Determines on base on the allergy test score for the given person, whether or not they're allergic to a given allergen(s).
         /// </summary>
@@ -24,7 +33,12 @@ namespace AllergyScore
         /// <returns>true if there is an allergy to this allergen, false otherwise.</returns>
         public bool IsAllergicTo(Allergens allergens)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (((Allergens)this.Score & allergens) != 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         /// <summary>
@@ -33,7 +47,19 @@ namespace AllergyScore
         /// <returns>Full list of allergies of the person with given allergy test score.</returns>
         public Allergens[] AllergensList()
         {
-            throw new NotImplementedException("You need to implement this method.");
+            int mask = 0b01;
+            var list = new List<Allergens>();
+            while (mask <= 128)
+            {
+                if ((mask & this.Score) != 0)
+                {
+                    list.Add((Allergens)mask);
+                }
+
+                mask <<= 1;
+            }
+
+            return list.ToArray();
         }
     }
 }
